71b71dbb6286caaa271e43a41afdc1e4000ad286
aunl <aunl@gft.com>
2020-04-07 13:46:22 +0200

    4.2: Input controls - Radio Buttons and Spinner Lists
    Signed-off-by: aunl <aunl@gft.com>

---
+++ Android_Developer_Fundamentals/Unit2/DroidCafeInput/app/src/main/java/com/example/android/droidcafeinput/OrderActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.droidcafeinput;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This activity handles radio buttons for choosing a delivery method for an
+ * order, a spinner for setting the label for a phone number, and EditText input
+ * controls.
+ */
+public class OrderActivity extends AppCompatActivity {
+
+    /**
+     * Sets the content view to activity_order, and gets the intent and its
+     * data. Also creates an array adapter and layout for a spinner.
+     *
+     * @param savedInstanceState Saved instance state bundle.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        // Get the intent and its data.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+    }
+
+    /**
+     * Displays the actual message in a toast message.
+     *
+     * @param message Message to display.
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+}
