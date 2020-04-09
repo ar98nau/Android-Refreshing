538d28e8d44386b4f9a4f09dd45c09257011bb4d
aunl <aunl@gft.com>
2020-04-09 12:01:19 +0200

    7.2: AsyncTask

---
+++ Android_Developer_Fundamentals/Unit3/WhoWroteIt/app/src/main/java/com/example/android/whowroteit/FetchBook.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.whowroteit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * FetchBook is an AsyncTask implementation that opens a network connection
+ * and queryies the Book Service API.
+ */
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    FetchBook(TextView titleText, TextView authorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+    /**
+     * Use the getBookInfo() method in the NetworkUtils class to make
+     * the connection in the background.
+     *
+     * @param strings String array containing the search data.
+     * @return Returns the JSON string from the Books API, or
+     * null if the connection failed.
+     */
+    @Override
+    protected String doInBackground(String... strings) {
+        return NetworkUtils.getBookInfo(strings[0]);
+    }
+
+    /**
+     * Handles the results on the UI thread. Gets the information from
+     * the JSON result and updates the views.
+     *
+     * @param s Result from the doInBackground() method containing the raw
+     *          JSON response, or null if it failed.
+     */
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                i++;
+            }
+
+            if (title != null && authors != null) {
+                mTitleText.get().setText(title);
+                mAuthorText.get().setText(authors);
+            } else {
+                mTitleText.get().setText(R.string.no_results);
+                mAuthorText.get().setText("");
+            }
+
+        } catch (Exception e) {
+            mTitleText.get().setText(R.string.no_results);
+            mAuthorText.get().setText("");
+            e.printStackTrace();
+        }
+
+    }
+}
