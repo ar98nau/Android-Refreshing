a37259ab61858735c339a80bce241774c3a73f28
aunl <aunl@gft.com>
2020-04-09 12:32:23 +0200

    7.2: AsyncTaskLoader

---
+++ Android_Developer_Fundamentals/Unit3/WhoWroteItLoader/app/src/main/java/com/example/android/whowroteit/BookLoader.java
@@ -0,0 +1,27 @@
+package com.example.android.whowroteit;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    public BookLoader(Context context, String queryString) {
+        super(context);
+        this.mQueryString = queryString;
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+}
